2e0634d441f405d1ccf61db07e38984ecdb71966
Gageu <unruh.gage@gmail.com>
2020-05-12 04:06:12 -0700

    collision fix

--- pa9/Collision.hpp
+++ pa9/Collision.hpp
@@ -60,7 +60,7 @@ namespace Collision {
     /// downloading the textures from the graphics card to memory -> SLOW!
     /// You can avoid this by using the "CreateTextureAndBitmask" function
     //////
-    bool PixelPerfectTest(const sf::Sprite& Object1, const sf::Sprite& Object2, sf::Uint8 AlphaLimit = 0);
+    bool PixelPerfectTest(const Entity& Object1, const Entity& Object2, sf::Uint8 AlphaLimit = 0);
 
     //////
     /// Replaces Texture::loadFromFile
